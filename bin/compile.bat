9a85c7f5bc2e5dd6e3ec3542934fc2150010b3c3
NasaNhak <nasanhak@gmail.com>
2018-10-04 20:26:34 +0530

    Adding compiler

---
+++ bin/compile.bat
@@ -0,0 +1,92 @@
+REM 
+
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_gmp_mission.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_hud.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_item.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mb.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_menu.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mission.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_quest.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_record.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tips.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tutorial.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_weapon.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\mb_staff_name.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_announce_log.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cassette.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cast_staff.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_common.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_dialog.eng.lng2.xml
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_emblem.eng.lng2.xml
+
+
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mbdvc\mb_dvc_top.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\common\title_sequence.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\free\f30050\f30050.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\free\f30150\f30150.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\free\f30250\f30250.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\heli\heli_common_script.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\mission2\init\init.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng.fpk.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_tnk.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_wav.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_wav_rocket.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_tnk.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_wav_cannon.fpkd.xml
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_wav_machinegun.fpkd.xml
+
+
+XCOPY ..\src\00\Assets\tpp\pack\mbdvc\mb_dvc_top.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\common\title_sequence.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\free\f3XCOPY ..\src\0050\f3XCOPY ..\src\0050.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\free\f30150\f30150.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\free\f30250\f30250.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\heli\heli_common_script.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\mission2\init\init.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\ui\lang\lang_default_data_eng.fpk .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_tnk.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_wav.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_east_wav_rocket.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_tnk.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_wav_cannon.fpkd .\
+XCOPY ..\src\00\Assets\tpp\pack\vehicle\veh_rl_west_wav_machinegun.fpkd .\
+
+
+XCOPY ..\src\00\init.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\EquipDevelopConstSetting.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\EquipDevelopFlowSetting.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\MbmCommonSetting.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\MbmCommonSetting20BaseResSec.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\MbmCommonSetting30Deploy.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\MbmCommonSetting40RewardDeploy.lua .\
+XCOPY ..\src\00\Assets\tpp\motherbase\script\MbmCommonSetting60DbPfLang.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\Tpp.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppDefine.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppDemo.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppEneFova.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppEnemy.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppHero.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppMain.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppMission.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppPlayer.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppReinforceBlock.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppResult.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppRevenge.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppStory.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppTelop.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppTerminal.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppUI.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TppWeather.lua .\
+XCOPY ..\src\00\Assets\tpp\script\list\TppMissionList.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\InfInspect.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPM.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMDamageParameterTables.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMEquipParameters.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMEquipParameterTables.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMLog.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMplayer2_camouf_param.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMSettings.lua .\
+XCOPY ..\src\00\Assets\tpp\script\lib\TUPPMStartup.lua .\
+
+
+pause
