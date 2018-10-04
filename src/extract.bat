1050e3be0f76d2ef28a2a4d9546b2faf0e334643
NasaNhak <nasanhak@gmail.com>
2018-10-04 20:00:36 +0530

    Added extraction tools

--- src/extract.bat
+++ src/extract.bat
@@ -1,26 +1,26 @@
 REM 
 
-REM "D:\Modding\MGSV\.Tools\GzsTool.v0.6.0\GzsTool.exe" .\00
-REM "D:\Modding\MGSV\.Tools\FoxTool.v0.2.5\FoxTool.exe" .\00 
+"..\.Tools\GzsTool.v0.6.0\GzsTool.exe" .\00
+"..\.Tools\FoxTool.v0.2.5\FoxTool.exe" .\00 
 
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_gmp_mission.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_hud.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_item.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mb.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_menu.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mission.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_quest.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_record.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tips.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tutorial.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_weapon.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\mb_staff_name.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_announce_log.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cassette.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cast_staff.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_common.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_dialog.eng.lng2
-"D:\Modding\MGSV\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_emblem.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_gmp_mission.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_hud.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_item.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mb.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_menu.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_mission.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_quest.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_record.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tips.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_tutorial.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_weapon.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\mb_staff_name.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_announce_log.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cassette.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_cast_staff.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_common.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_dialog.eng.lng2
+"..\.Tools\FoxEngine.TranslationToolv0.2.5\LangTool.exe" .\00\Assets\tpp\pack\ui\lang\lang_default_data_eng_fpk\Assets\tpp\lang\ui\tpp_emblem.eng.lng2
 
 
 pause
